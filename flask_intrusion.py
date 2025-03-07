0e95872fd24e3865e28c6645d54439a822757d68
VALLIPALLI BHANU CHAITANYA <130381125+bhanuchaitu@users.noreply.github.com>
2025-03-07 20:27:56 +0530

    Flask_intrusion.py

--- flask_intrusion.py
+++ flask_intrusion.py
@@ -1,100 +1,142 @@
-from flask import Flask, jsonify, request
 import numpy as np
+from flask import Flask, request, jsonify, render_template
 import joblib
+import sqlite3
+
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+from sklearn import metrics 
+import warnings
+import pickle
+import pandas as pd
+import numpy as np
+import pickle
+import sqlite3
+import random
 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
-import xgboost as xgb
+import smtplib 
+from email.message import EmailMessage
+from datetime import datetime
 
-# to display all column of datapoints
-pd.set_option('display.max_columns', None)
+warnings.filterwarnings('ignore')
 
-import flask
+
+
 app = Flask(__name__)
 
 
-X=pd.read_csv('final_test.csv')
-
-X = X[[ 'duration' ,'src_bytes','dst_bytes','wrong_fragment',
-'urgent','hot','num_failed_logins','num_compromised','num_root','num_file_creations','num_shells',
-'num_access_files','num_outbound_cmds','count','srv_count','serror_rate','srv_serror_rate',
-'rerror_rate','srv_rerror_rate','same_srv_rate','diff_srv_rate','srv_diff_host_rate','dst_host_count',
-'dst_host_srv_count','dst_host_same_srv_rate','dst_host_diff_srv_rate',
-'dst_host_same_src_port_rate','dst_host_srv_diff_host_rate','dst_host_serror_rate',
-'dst_host_srv_serror_rate','dst_host_rerror_rate','dst_host_srv_rerror_rate','protocol_type', 'service' , 'flag']]
-
-def data_preprocessing(X):
-    
-    """
-    Normal=0 ,Probing=1,DOS=2,U2R=3,R2L=4
-    
-    """
-    
-    continuous_features=["duration","src_bytes",
-    "dst_bytes","wrong_fragment","urgent","hot","num_failed_logins","num_compromised","num_root",
-    "num_file_creations","num_shells","num_access_files","num_outbound_cmds","count","srv_count","serror_rate",
-    "srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate",
-    "diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count",
-    "dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate",
-    "dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate",
-    "dst_host_rerror_rate","dst_host_srv_rerror_rate"]
-
-    
-    #LabelEncoder() function Encode target labels with value between 0 and n_classes-1
-    le = LabelEncoder()
-
-    # protocol
-    X_protocol_type = le.fit_transform(X['protocol_type'].values)
-    X_service = le.fit_transform(X['service'].values)
-    X_flag = le.fit_transform(X['flag'].values)
-
-    X_main = X[continuous_features]
-    
-    X_main['protocol_type'] = X_protocol_type
-    X_main['service'] = X_service
-    X_main['flag'] = X_flag
-    
-    
-    return X_main
-
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/index')
 def index():
-    return flask.render_template('index.html')
+    return render_template('index.html')
 
-binary_model = joblib.load('RandomForestClassifier_over_sampling_SMOTETomek.joblib')
-multiclass_model = joblib.load('XGBClassifier_multiclass_without_sampling.joblib')
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/home')
+def home():
+	return render_template('home.html')
+
+
+@app.route('/logon')
+def logon():
+	return render_template('signup.html')
+
+@app.route('/login')
+def login():
+	return render_template('signin.html')
+
+
+
+@app.route("/signup")
+def signup():
+    global otp, username, name, email, number, password
+    username = request.args.get('user','')
+    name = request.args.get('name','')
+    email = request.args.get('email','')
+    number = request.args.get('mobile','')
+    password = request.args.get('password','')
+    otp = random.randint(1000,5000)
+    print(otp)
+    msg = EmailMessage()
+    msg.set_content("Your OTP is : "+str(otp))
+    msg['Subject'] = 'OTP'
+    msg['From'] = "evotingotp4@gmail.com"
+    msg['To'] = email
     
     
-    binary_model_pred = binary_model.predict(X)
-    
-    multiclass_model_pred = multiclass_model.predict(X)
-    
-    i = [0 if j == 0 else 1 for j in binary_model_pred]
-    j=int(i[0])
-    
-    k = [l for l in multiclass_model_pred]   
-    l=int(k[0]) 
-    
-    if j == 1:
-        
-        return jsonify({'The Result in Instrusion Singal': l })
-    
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login("evotingotp4@gmail.com", "xowpojqyiygprhgr")
+    s.send_message(msg)
+    s.quit()
+    return render_template("val.html")
+
+@app.route('/predict1', methods=['POST'])
+def predict1():
+    global otp, username, name, email, number, password
+    if request.method == 'POST':
+        message = request.form['message']
+        print(message)
+        if int(message) == otp:
+            print("TRUE")
+            con = sqlite3.connect('signup.db')
+            cur = con.cursor()
+            cur.execute("insert into `info` (`user`,`email`, `password`,`mobile`,`name`) VALUES (?, ?, ?, ?, ?)",(username,email,password,number,name))
+            con.commit()
+            con.close()
+            return render_template("signin.html")
+    return render_template("signup.html")
+
+@app.route("/signin")
+def signin():
+
+    mail1 = request.args.get('user','')
+    password1 = request.args.get('password','')
+    con = sqlite3.connect('signup.db')
+    cur = con.cursor()
+    cur.execute("select `user`, `password` from info where `user` = ? AND `password` = ?",(mail1,password1,))
+    data = cur.fetchone()
+
+    if data == None:
+        return render_template("signin.html")    
+
+    elif mail1 == str(data[0]) and password1 == str(data[1]):
+        return render_template("home.html")
     else:
-        
-        return jsonify({'The Result in Non Instrusion Singal': j })
+        return render_template("signin.html")
+
+@app.route("/notebook")
+def notebook1():
+    return render_template("Notebook.html")
+
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features= [float(x) for x in request.form.values()]
+    print(int_features,len(int_features))
+    final4=[np.array(int_features)]
+    model = joblib.load('model_kdd.sav')
+    predict = model.predict(final4)
+
+    if predict==0:
+        output='There is No Attack Detected and Its Normal!'
+    elif predict==1:
+        output='Attack is Detected and its Probe Attack!'
+    elif predict==2:
+        output='Attack is Detected and its R2L Attack!'
+    elif predict==3:
+        output='Attack is Detected and its U2R Attack!'
+    elif predict==4:
+        output='Attack is Detected and its DOS Attack!'
     
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    
+
+    return render_template('prediction.html', output=output)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
